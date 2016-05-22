a2b76c5509972fd416f0f612e2bf43773e0f68d5
madD7 <sdhaval.3003@gmail.com>
2016-05-22 18:54:49 +0530

    Propogation of Exception to the caller method

--- exceptionHandling/exceptionHandling/XceptionHandling.java
+++ exceptionHandling/exceptionHandling/XceptionHandling.java
@@ -21,7 +21,18 @@ class Class1
 	static void method1()
 	{
 		System.out.println("Entering  Class1.method1 ");
-        Class2.method1();
+		try 
+		{
+          	System.out.println("Entering try");
+            
+          	Class2.method1();
+            
+            System.out.println("This line is not executed"); 
+		}catch (ArithmeticException e) 
+		{
+			System.out.println("Arithmetic Exception Caught ");
+		}
+		
         System.out.println("Leaving  Class1.method1 ");
 	}
 }
@@ -34,17 +45,12 @@ class Class2
 		int a=1, b=2, c=0;
 		System.out.println("Entering  Class2.method1 ");
 		
-		try {
-            System.out.println("Entering try");
-            a = b / c;
-            
-			// The code below the line where execption occurs is not executed
-            // The flow of control jumps to the catch block & starts execution from there onwards
-            System.out.println("This line is not executed"); 
-        } catch (ArithmeticException e) {
-            System.out.println("Arithmetic Exception Caught ");
-        }		
+		// Generating a Divide-by-Zero exception
+		a = b / c;
+      	// The exception is propogated to Class1.method1 as the catch block is in that method 
 		
+		
+		System.out.println("Some random print message to check whether it executes or not ");
 		System.out.println("Leaving  Class2.method1 ");
 	}
 }
