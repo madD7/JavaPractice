a9a7de865f174de39c9ce7d37b6dba5d65865101
madD7 <sdhaval.3003@gmail.com>
2016-05-29 22:35:14 +0530

    Primitive data types in java

--- structuredProgramming/valuetypereftype.java
+++ structuredProgramming/valuetypereftype.java
@@ -19,6 +19,8 @@ package structuredProgramming;
  * multiple reference variables can refer to the same value ( memory block ). 
  * Consequently, changing value via one variable gets reflected when accessing value via other variable. 
  * Immutability of string eliminates this scenario.
+ * 
+ * Primitive types and their state space and size
  */
 
 
@@ -60,6 +62,43 @@ public class ValueTypeRefType {
         
         System.out.println("str1 = " + str1);
         System.out.println("str2 = " + str2);
+         	
+        
+        System.out.println("Demonstration of Primitive types in Java, theirs state space and sizes");
+        
+        /*
+         * Wrapper Classes of the respective primitive types are used below
+         */
+               
+        System.out.println("The size of Byte in bits is " + Byte.SIZE + "bits");
+        System.out.println("The size of Byte in bytes is " + Byte.BYTES + "bytes");
+        System.out.println("The MAX_VALUE of Byte in bytes is " + Byte.MAX_VALUE);
+        System.out.println("The MIN_VALUE of Byte in bytes is " + Byte.MIN_VALUE);
+        
+        System.out.println("The size of Character in bits is " + Character.SIZE + "bits");
+        System.out.println("The size of Character in bytes is " + Character.BYTES + "bytes");
+        System.out.println("The MAX_VALUE of Character in bytes is " + (int)Character.MAX_VALUE);
+        System.out.println("The MIN_VALUE of Character in bytes is " + (int)Character.MIN_VALUE);
+        
+        System.out.println("The size of Short in bits is " + Short.SIZE + "bits");
+        System.out.println("The size of Short in bytes is " + Short.BYTES + "bytes");
+        System.out.println("The MAX_VALUE of Short in bytes is " + Short.MAX_VALUE);
+        System.out.println("The MIN_VALUE of Short in bytes is " + Short.MIN_VALUE);
+        
+        System.out.println("The size of Integer in bits is " + Integer.SIZE + "bits");
+        System.out.println("The size of Integer in bytes is " + Integer.BYTES + "bytes");
+        System.out.println("The MAX_VALUE of Integer in bytes is " + Integer.MAX_VALUE);
+        System.out.println("The MIN_VALUE of Integer in bytes is " + Integer.MIN_VALUE);
+        
+        System.out.println("The size of Long in bits is " + Long.SIZE + "bits");
+        System.out.println("The size of Long in bytes is " + Long.BYTES + "bytes");
+        System.out.println("The MAX_VALUE of Long in bytes is " + Long.MAX_VALUE);
+        System.out.println("The MIN_VALUE of Long in bytes is " + Long.MIN_VALUE);
+        
+        System.out.println("The size of Float in bits is " + Float.SIZE + "bits");
+        System.out.println("The size of Float in bytes is " + Float.BYTES + "bytes");
+        System.out.println("The MAX_VALUE of Float in bytes is " + Float.MAX_VALUE);
+        System.out.println("The MIN_VALUE of Float in bytes is " + Float.MIN_VALUE);
 	}
 
 }
