4dd9fdc3e48199b4a79935de568064c6c6f6e537
madD7 <sdhaval.3003@gmail.com>
2016-05-29 10:41:18 +0530

    structured programming - calculator example - learning value type and reference type

---
+++ structuredProgramming/valuetypereftype.java
@@ -0,0 +1,21 @@
+package structuredProgramming;
+
+/**
+ * @author madD7
+ * a simple java program
+ */
+
+// Demonstrate value type variable and reference type variable
+
+
+class valuetypereftype {
+
+	public static void main(String args)
+	{
+		int valuetype=10;
+		String reftype="Hello World";
+		
+		System.out.println("Value of valuetype: " + valuetype);
+		System.out.println(reftype);
+	}
+}
