4c101ec3af7482f58cfa8807beec6e21e24ef54d
madD7 <sdhaval.3003@gmail.com>
2016-05-29 11:18:18 +0530

    reference type theory explained

--- structuredProgramming/valuetypereftype.java
+++ structuredProgramming/valuetypereftype.java
@@ -3,6 +3,13 @@ package structuredProgramming;
 /**
  * @author madD7
  * Calculator Program - Demonstrate value type & Reference type
+ * 
+ * Value type variables hold the value itself 
+ * whereas reference type variables hold reference to the value
+ * Reference to object is typically machine address of that variable
+ * Reference is neither a Pointer to object, nor its copy or copy of pointer either.
+ * Reference is the object itself.
+ * In compiler language, reference is an 'lvalue', something that can appear at the LHS of an assignment operator 
  */
 
 
@@ -14,8 +21,7 @@ public class ValueTypeRefType {
 		String reftype = "Hello world";
 		
 		System.out.println("value of Value type variable vlutyp = " + vlutype);
-		System.out.println(reftype);
-				
+		System.out.println(reftype);			
 	}
 
 }
