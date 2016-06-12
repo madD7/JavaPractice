fea3f96254002f5ac533ccbeb13ea845b86d5edc
madD7 <sdhaval.3003@gmail.com>
2016-06-12 19:05:51 +0530

    Thinking pattern change for a C programmer towards a datastructure. DElegating responsibility to object.

--- GeometryPoint/ObjectCreation.java
+++ GeometryPoint/ObjectCreation.java
@@ -16,6 +16,22 @@ class Point
 {
 	int x;		// Instance variables
 	int y;
+	
+	/**
+	 * Shift in thought process for a C programmer
+	 * Delegating work to  the object. 
+	 * Hence, objects are capable of knowing as well as doing
+	 */
+	void movePoint(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+	
+	void print()
+	{
+		System.out.println("Co-ordinates x:"+this.x+", y:"+this.y);
+	}
 }
 
 public class ObjectCreation{
@@ -31,8 +47,13 @@ public class ObjectCreation{
 		p2.x = 3;
 		p2.y = 5;
 		
-		System.out.println("Co-ordinates of p1: x:"+p1.x+", y:"+p1.y);
-		System.out.println("Co-ordinates of p2: x:"+p2.x+", y:"+p2.y);
+		// Passing message 'print' to the object p1. p1 will print its co-ordinates.
+		// 'print' work is delegated to object p1
+		p1.print();
+		p2.print();
+		
+		p1.movePoint(6, 7);
+		p1.print();
 	}
 
 }
