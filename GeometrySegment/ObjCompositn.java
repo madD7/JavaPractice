2f2f70596b3dd08729300678a7b93847e216531b
madD7 <sdhaval.3003@gmail.com>
2016-06-26 01:19:43 +0530

    Object Linking

--- GeometrySegment/ObjCompositn.java
+++ GeometrySegment/ObjCompositn.java
@@ -15,7 +15,7 @@ class Point
 {
 	int x=0;
 	int y=0;
-	String name;
+	private String name;
 	
 	Point(int x, int y, String name)
 	{
@@ -24,7 +24,12 @@ class Point
 		this.name = name;
 	}
 	
-	void printPoint()
+	Point(String name)
+	{
+		this.name = name;
+	}
+	
+	void print()
 	{
 		System.out.println("Co-ordinates of " + this.name + " x:"+this.x+", y:"+this.y);
 	}
@@ -42,7 +47,7 @@ class SimpleSegment
 	int y1=0;
 	int x2=1;
 	int y2=1;
-	String name;
+	private String name;
 	
 	SimpleSegment(int x1, int y1, int x2, int y2, String name)
 	{
@@ -54,10 +59,50 @@ class SimpleSegment
 		this.name = name;
 	}
 	
-	void printSimpleSegment()
+	void print()
 	{
 		System.out.print("Co-ordinates of " + this.name + " x1:"+this.x1+", y1:"+this.y1);
 		System.out.println(" and" + " x2:"+this.x2+", y2:"+this.y2);
+	}
+}
+
+
+/**
+ * The composite class has two point objects, actually handles to the objects of Point class
+ * The objects handles are initialized to null by the initializer. 
+ * Such object composition is known as obj linking, where obj handles are present in an object.
+ * Object inside an object, i.e. object embedding is not allowed in java
+ * Object linking allows lazy initialization
+ * 
+ * When an object of type CompositeSegment is created, only the container object is created
+ * but not its component objects (start and end).
+ * Hence in object linking, proper creation and linking is required.
+ */
+class CompositeSegment
+{
+	Point start;
+	Point end;
+	private String name;		// String name not compulsory 
+	
+	/** The following execution will result to upfront initialization, i.e. initialization during creation
+	 * Point start = new Point();
+	 * Point end = new Point();
+	 */
+	
+	/**
+	 * I would prefer writing a constructor instead of upfront initialization
+	 * */
+	CompositeSegment(String name)
+	{
+		this.name = name;
+		this.start = new Point("Start pt of "+name );
+		this.end = new Point("End pt of "+name );
+	}
+	
+	void print()
+	{
+		System.out.print("Co-ordinates of " + this.name + " Start point: x="+this.start.x +", y="+this.start.y);
+		System.out.println(" and" + " End point: x="+this.end.x +", y="+this.end.y);
 	}
 }
 
@@ -68,7 +113,11 @@ public class ObjCompositn {
 	public static void main(String[] args)
 	{
 		SimpleSegment S1 = new SimpleSegment(0, 0, 5, 5, "S1");
+		CompositeSegment S2 = new CompositeSegment("S2");
 		
-		S1.printSimpleSegment();
+		S1.print();
+		S2.print();
+		S2.start.print();
+		S2.end.print();
 	}
 }
