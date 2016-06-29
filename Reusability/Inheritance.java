f8e21a7817aac9a1070b5190dc214928ed8bb09c
madD7 <sdhaval.3003@gmail.com>
2016-06-30 01:11:36 +0530

    liskovs' principle of substitution

--- Reusability/Inheritance.java
+++ Reusability/Inheritance.java
@@ -1,6 +1,8 @@
 package reusability;
 
-
+/**
+ * Base class is also known as Parent class
+ */
 class BaseClass
 {
 	int a;
@@ -14,11 +16,20 @@ class BaseClass
 }
 
 
-class ChildClass extends BaseClass
+/**
+ * Child class is also known as DerivedClass
+ * 
+ */
+class DerivedClass extends BaseClass
 {
 	int c;
 	int d;
 	
+	/**
+	 * The child class inherits parent
+	 * Hence variables present in parent class are also present in child class.
+	 * This is by inheritance. 
+	 */
 	void printAll()
 	{
 		System.out.println("Value of a is " + this.a);
@@ -34,7 +45,7 @@ public class Inheritance {
 	void demoInheritance()
 	{
 		BaseClass bObj = new BaseClass();
-		ChildClass cObj = new ChildClass();
+		DerivedClass cObj = new DerivedClass();
 		
 		bObj.a = 10;
 		bObj.b = 20;
@@ -51,5 +62,44 @@ public class Inheritance {
 		 * */
 		cObj.print();
 		cObj.printAll();
+		
+		/* A parent obj can be passed to a parent handle - obvious */
+		printBaseClass(bObj);
+		
+		/* A child obj can be passed to a child handle - obvious */
+		printDerivedClass(cObj);
+		
+		/** A child obj can be passed to a parent handle - Liskov's principle of substitution
+		 * This is allowed because child obj has all the variables of parent (child has inherited from parent) 
+		 * */
+		printBaseClass(cObj);
+		
+		/** A child obj can be passed to a parent handle - NOT ALLOWED
+		 * Because, though child has all the properties of parent onj, vice-versa is not true
+		 * i.e. the parent may lack all properties (variables & methods) contained in child.
+		 * Hence a child handle cant access variables & methods present only in child (that are not in parent) 
+		 */
+		//printDerivedClass(bObj);
+		
 	}
+	
+	
+	/** The method accepts parent class obj and prints its variables
+	 * */
+	static void printBaseClass(BaseClass var)
+	{
+		System.out.println("Value of a is " + var.a);
+		System.out.println("Value of b is " + var.b);
+	}
+	
+	/** The method accepts child class obj and prints its variables
+	 * */
+	static void printDerivedClass(DerivedClass var)
+	{
+		System.out.println("Value of a is " + var.a);
+		System.out.println("Value of b is " + var.b);
+		System.out.println("Value of c is " + var.c);
+		System.out.println("Value of d is " + var.d);
+	}
+	
 }
